58984126e2dcc269a953c8641e2b093c1964a7ad
DV <dvislearning@gmail.com>
2016-09-30 13:00:32 -0700

    I1: generates a random number

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,5 +1,10 @@
 require 'sinatra'
+require 'sinatra/reloader'
 
 get '/' do
-	'Hello World!'
+	random_number = rand(101)
+	'The SECRET NUMBER is ' + random_number.to_s
 end
+
+#Sinatra/reloader gem allows you to reload your page with changes without
+#having to reload the server.
