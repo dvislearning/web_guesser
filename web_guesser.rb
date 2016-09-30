823599b3e0437a349ae27a97358cd5e353db477f
DV <dvislearning@gmail.com>
2016-09-30 14:47:14 -0700

    I2: uses an ERB template and HTML

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,9 +1,10 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
+number = rand(101)
+
 get '/' do
-	random_number = rand(101)
-	'The SECRET NUMBER is ' + random_number.to_s
+	erb :index, :locals => {:number => number}
 end
 
 #Sinatra/reloader gem allows you to reload your page with changes without
